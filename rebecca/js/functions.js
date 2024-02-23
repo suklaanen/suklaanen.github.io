5ac830fbcf466dc75f2bd38de8c433fc685fbca7
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-02-23 14:55:15 +0200

    addmyio

---
+++ rebecca/js/functions.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            
+            var targetId = this.getAttribute('href').substring(1);
+            
+            var targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
