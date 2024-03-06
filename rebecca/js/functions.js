3d68f8c434c3f25fe3eb8850dc6565c2281d74e4
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-03-06 23:26:25 +0200

    newUi

--- rebecca/js/functions.js
+++ rebecca/js/functions.js
@@ -14,4 +14,33 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+    var quotes = [
+        "\" Asiat pitää tehdä kunnolla. \"",
+        "\" Saattasin olla teille se lupaava juniori. \"",
+        "\" Mussa palaa halu onnistua. \"",
+        "\" Jos haluu pärjätä, niin työstään pitää tykätä. \"",
+        "\" Ei mikään turha lupaus. \"",
+        "\" Tänään ja huomenna. \"",
+    ];
+
+    var randomIndex = Math.floor(Math.random() * quotes.length);
+    var randomQuote = quotes[randomIndex];
+
+    var quoteElement = document.getElementById('quote');
+    quoteElement.textContent = randomQuote;
+    
+});
+
+const colorbar = document.getElementById('colorbar');
+
+const scrollThreshold = 100;
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > scrollThreshold) {
+        colorbar.classList.add('scrolled');
+    } else {
+        colorbar.classList.remove('scrolled');
+    }
 });
